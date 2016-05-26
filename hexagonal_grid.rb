3a09741b5aafe2f9a6073309c224b820204c783b
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-05-26 16:27:20 -0700

    Add an rspec file

--- hexagonal_grid.rb
+++ hexagonal_grid.rb
@@ -1,61 +1,50 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 # STDIN.each { |el| puts el.class } # => They're all strings.
 # STDOUT = [] # => don't do this, b/c STDOUT is already defined
+class Hex_Solution
+  def solution test_cases
+    result = []
 
-result = []
+    input = []
+    test_cases.each { |el| input << el } # Change test_cases to STDIN for submission.
 
-input = []
-STDIN.each { |el| input << el }
+    test_case_quantity = input.shift.to_i
 
-test_case_quantity = input.shift.to_i
+    test_case_quantity.times do
+      grid_length = input.shift
+      row1 = input.shift.chars
+      row2 = input.shift.chars
 
-test_case_quantity.times do
-    grid_length = input.shift
-    row1 = input.shift.chars
-    row2 = input.shift.chars
+      # r = red, m = maroon, y = yellow
+      # 1 = black, 0 != black
 
-    # r = red, m = maroon, y = yellow
+      i, j = 0, 0
+      while i < row1.length && j < row2.length
 
-    i, j = 0, 0
-    while i < row1.length && j < row2.length
-        # james, pick up here. Loop an assessment algorithm, which starts with i top-left of j. Both cells unassessed.
+        # Checking only for the impossible scenarios (assuming they are finite)
+        # IF none are found, push "YES" into result.
+        # ELSE push "NO" into result.
+        # Impossible scenarios are as follows:
+        # These 4 scenarios need another black cell horizontally OR
+        # vertically adjacent OR on another row.
+        #   [0, 0]     [0, 0]     [1, 0]     [0, 1]
+        #     [1, 0]     [0, 1]     [0, 0]     [0, 0]
+
+        # Now I'm wondering if there just has to be an even number of black cells
+        # dispersed in any fashion other than the corner cases...
+        # Must write some tests!
+
         do
-            # Is the first cell of the top row black?
-            if row1[i] == 1
-                i += 1
-                # Is there a black cell top-left of another black cell?
-                if row2[j] == 1
-                    result.push "NO"
-                end
-                # It's black top-left of clear.
-                j += 1
-                if row2[j] == 1
-                    result.push "NO"
-                end
-                # Lay a lower yellow (really imaginary, by incrementing j thusly)
-                j += 1
-                # Now it's black top-left, yellow lower-right
-                # Is it [1, 1] ?
-                #         [y, y]
-                if row1[i] == 1
-                    i += 1
-                    break # Start loop over. Whoo-hoo!
 
-                # Is it [1, 0...] ?
-                #         [y, y...]
-                elsif
-
-                end
-            # It's a NOGO.
-            else
-                result.push "NO"
-            end
         end
         # increment i and j somewhere
+      end
+
+
     end
 
+    # STDOUT is just whatever you puts
+    result.each { |answer| puts answer } # what about last one? newline char?
 
+  end
 end
-
-# STDOUT is just whatever you puts
-result.each { |answer| puts answer } # what about last one? newline char?
---
+++ spec/hex_spec.rb
@@ -0,0 +1,7 @@
+require './hexagonal_grid.rb'
+
+describe Hex_Solution do
+  it 'works' do
+    1 == 1
+  end
+end
