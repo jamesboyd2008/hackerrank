43f31e981eec1bf3f73d0cbc9f411ee8bc1623c5
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-05-26 18:38:23 -0700

    Update specs for syntax

--- hexagonal_grid.rb
+++ hexagonal_grid.rb
@@ -1,30 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 # STDIN.each { |el| puts el.class } # => They're all strings.
 # STDOUT = [] # => don't do this, b/c STDOUT is already defined
-class Hex_Solution
-  def solution test_cases
-    result = []
 
-    input = []
-    test_cases.each { |el| input << el } # Change test_cases to STDIN for submission.
-
-    test_case_quantity = input.shift.to_i
-
-    test_case_quantity.times do |case|
-      grid_length = input.shift.to_i
-      row1 = input.shift.chars
-      row2 = input.shift.chars
-
-      # r = red, m = maroon, y = yellow
-      # 1 = black, 0 != black
-
-      i, j = 0, 0
-      if (row1.count("1") + row2.count("1")).even?
-        # Check for corner cases. IF none are met, THEN push "YES" into result.
-        if true
-          result.push "YES"
-        end
-      end
 
         # Checking only for the impossible scenarios (assuming they are finite)
         # IF none are found, push "YES" into result.
@@ -38,7 +15,35 @@ class Hex_Solution
         # dispersed in any fashion other than the corner cases...
         # Must write some tests!
 
-      result
+        # r = red, m = maroon, y = yellow
+        # 1 = black, 0 != black
+
+class HexSolution
+  def solution test_cases
+    result = []
+
+    input = []
+    test_cases.each { |el| input << el }
+
+    test_case_quantity = input.shift.to_i
+
+    test_case_quantity.times do |test_number|
+      grid_length = input.shift.to_i
+      row1 = input.shift.chars
+      row2 = input.shift.chars
+
+
+
+      i, j = 0, 0
+      if (row1.count("1") + row2.count("1")).even?
+        # Check for corner cases. IF none are met, THEN push "YES" into result.
+        # if true # pick up here. Look for better corner cases. Diagonals and such.
+          result.push "YES"
+        # end
+      end
+      if result.length < test_number + 1
+        result.push "NO"
+      end
     end
 
     # STDOUT is just whatever you puts
--- spec/hex_spec.rb
+++ spec/hex_spec.rb
@@ -1,10 +1,10 @@
 require './hexagonal_grid.rb'
 
-describe Hex_Solution do
-
-  @tests = ["6", "6", "010000", "000010", "2", "00", "00", "2", "00", "10", "2",
-    "00", "01", "2", "00", "11", "2", "10", "00"]
-  @answers = ["YES", "YES", "NO", "NO", "YES", "NO"]
+describe HexSolution do
+  hexy = HexSolution.new
+  tests = ["6", "6", "010000", "000010", "2", "00", "00", "2", "00", "10", "2",
+          "00", "01", "2", "00", "11", "2", "10", "00"]
+  answers = ["YES", "YES", "NO", "NO", "YES", "NO"]
 
   it 'specs work' do
     expect(1).to eq(1)
@@ -15,7 +15,7 @@ describe Hex_Solution do
   end
 
   it "returns the correct results" do
-    expect(solution(@tests)).to eq(@answers)
+    expect(hexy.solution(tests)).to eq(answers)
   end
 
 end
