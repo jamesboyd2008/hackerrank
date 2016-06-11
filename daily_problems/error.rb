dc85404b6163dce6cfa99bb56d50a1dccb972367
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-06-10 18:17:41 -0700

    Update solution to not use conditionals (as per instructions)

--- daily_problems/error.rb
+++ daily_problems/error.rb
@@ -1,5 +1,15 @@
 # convert a string into an integer.
 # from https://www.hackerrank.com/challenges/30-exceptions-string-to-integer
 
-s = gets.strip
-puts ((s.to_i != 0 || s == '0') ? s.to_i : 'Bad String')
+# s = gets.strip
+# puts ((s.to_i != 0 || s == '0') ? s.to_i : 'Bad String')
+
+# Here's a solution that doesn't use conditionals, i think.
+
+S = gets.strip
+
+begin
+    puts Integer S
+rescue
+    puts 'Bad String'
+end
